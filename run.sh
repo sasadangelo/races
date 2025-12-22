99f4407d2bd5b9015c88f1927e1e163beba1963f
Salvatore D'Angelo <sasadangelo@gmail.com>
2025-12-22 11:09:10 +0100

    Modernize UI with Bootstrap 5

--- run.sh
+++ run.sh
@@ -1,2 +1,12 @@
+#!/bin/bash
+
+# Activate virtual environment if it exists
+if [ -d ".venv" ]; then
+    source .venv/bin/activate
+fi
+
+# Set Flask app and run in debug mode
 export FLASK_APP=races
 flask run --debug
+
+# Made with Bob
